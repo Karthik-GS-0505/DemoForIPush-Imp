6bd442f4c9c3e491f5f44158e0e467f718437e46
Karthik <Karthik@HP>
2024-12-18 16:37:09 +0530

    ge

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -2,8 +2,16 @@
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Sample");
-
-	}
-
+		int[] arr = {5,4,7,2,1,9,15};
+		int max = 0;
+		
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			if(max<arr[i])
+			{
+				max = arr[i];
+			}
+		}
+		System.out.println("max ="+max);
+}
 }
